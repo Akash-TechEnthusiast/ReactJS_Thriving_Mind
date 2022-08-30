52387188a5e0e82e634612f40d6e9ff62f1e16e2
Husen <husenaiah.gandham@kagamierp.com>
2022-08-30 16:21:01 +0530

    #5353 login page changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import Home from "./pages/home/Home";
+import NoPage from "./pages/nopage/NoPage";
+import Login from "./pages/login/Login";
+import React, { useState } from 'react';
 // import React, { Component }  from 'react';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className="App">
-     <Home/>
+
+
+      <BrowserRouter>
+        <Routes>
+
+          <Route path="/hello" element={<Home />}>
+
+            <Route index element={<Home />} />
+            <Route path="home" element={<Home />} />
+            <Route path="contact" element={<Home />} />
+            <Route path="*" element={<NoPage />} />
+
+          </Route>
+
+        </Routes>
+      </BrowserRouter>
+
+
     </div>
   );
 }
 
 export default App;
+//      <Home />
